66eb721ddb5368ecd0a31cb68b81d4341833a697
Administrator <aj.villegasc@uea.edu.ec>
2024-08-30 11:45:26 -0500

    Matriz1

---
+++ matriz1.py
@@ -0,0 +1,21 @@
+matriz = [
+    [10, 20, 30],
+    [40, 50, 60],
+    [70, 80, 90]
+]
+
+def buscar_valor(matriz, valor):
+    for i in range(len(matriz)):
+        for j in range(len(matriz[i])):
+            if matriz[i][j] == valor:
+                return (i, j)
+    return None
+
+valor_a_buscar = 50
+
+resultado = buscar_valor(matriz, valor_a_buscar)
+
+if resultado:
+    print(f"Valor {valor_a_buscar} encontrado en la posición {resultado}.")
+else:
+    print(f"Valor {valor_a_buscar} no encontrado en la matriz.")
